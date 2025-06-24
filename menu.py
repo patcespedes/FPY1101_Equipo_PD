28a97c02e3135e9a9a61190673c63e0b8eef1341
Patricio Céspedes <pat.cespedes@duocuc.cl>
2025-06-24 18:22:15 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,3 +1,6 @@
+def datos_patricio():
+    print("Mi nombre es Patricio Céspedes y tengo 30 años.")
+
 # Menú base del programa
 while True:
     print("\n--- MENÚ PRINCIPAL ---")
@@ -12,7 +15,7 @@ while True:
     elif op == "1":
         pass # Aquí se llamará a la función del integrante 1
     elif op == "2":
-        pass # Aquí se llamará a la función del integrante 2
+        datos_patricio()
     elif op == "3":
         pass # Aquí se llamará a la función del integrante 3
     else:
